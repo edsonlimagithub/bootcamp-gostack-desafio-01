85075fd19905a2630c92cc43f9512209d95cd2bb
Edson Lima <edsonlima2178@gmail.com>
2019-06-25 21:22:52 -0300

    final project

--- index.js
+++ index.js
@@ -6,12 +6,31 @@ const app = express();
 
 app.use(express.json());
 
-http.createServer(app).listen(3000);
-
+let requestNumber = 0;
 projects = []  
 
+function countRequest(req, res, next){
+    requestNumber++;
+
+    console.log(`Requisição número: ${requestNumber}`)
+
+    next()
+}
+
+function validadeProject(req, res, next){
+    const { id } = req.params;
+    const project = projects.find(p => p.id === id)
+
+    if(!project){
+        return res.json({erro: "Project not found"})
+    }
+
+    next();
+}
+
+app.use(countRequest);
 //List projetos
-app.get('/', (req, res) => {
+app.get('/projects', (req, res) => {
     return res.json(projects);
 });
 
@@ -25,13 +44,56 @@ app.post('/projects', (req, res) => {
     }
     projects.push(project)
 
-    return res.json({ projects })
+    return res.json({ project })
 });
 
-//List project
-app.get('/project/:id', (req, res) => {
-    projects.map()
+//Show project
+app.get('/projects/:id', validadeProject, (req, res) => {
+    const { id } = req.params;
+
+    const project = projects.find(p => p.id === id)
+
+    return res.json(project);
+
 })
 
+//Upodate
+app.put('/projects/:id', (req, res) => {
+    const id = req.params.id;
+    const { title } = req.body;
+
+    const project = projects.find(p => p.id === id)
+
+    project.title = title 
+
+    return res.json(project)
+})
+
+//Delete
+app.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const index = projects.findIndex(p => p.id === id)
+
+    projects.splice(index, 1);
+
+    return res.send();
+})
+
+//Add tasks
+app.post('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params;
+
+    const { task } = req.body;
+
+    const project = projects.find(p => p.id === id)
+
+    project.tasks.push(task)
+
+    res.json(project)
+
+})
+
+
+http.createServer(app).listen(3000);
 
 
